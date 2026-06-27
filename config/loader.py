4192727957f7bde95499237f8fe8cb6410c5b1d9
itsmatin-stack <itsmatin@gmail.com>
2026-06-27 13:19:33 +0530

    Delete all files from remote

--- config/loader.py
+++
@@ -1,6 +0,0 @@
-import json, os
-
-def load_config():
-    path = os.path.join(os.path.dirname(__file__), "settings.json")
-    with open(path, "r", encoding="utf-8") as f:
-        return json.load(f)
